f8b9084a3b741120dd607a02f51fd57d7e24c48b
LucasG234 <lucasgates234@gmail.com>
2020-07-06 16:10:39 -0500

    Added LoginActivity

--- app/src/main/java/com/lucasg234/parstagram/LoginActivity.java
+++ app/src/main/java/com/lucasg234/parstagram/LoginActivity.java
@@ -3,12 +3,35 @@ package com.lucasg234.parstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.lucasg234.parstagram.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    
+    private ActivityLoginBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        
+        mBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mBinding.loginUsername.getText().toString();
+                String password = mBinding.loginPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to log in user: " + username);
     }
 }
